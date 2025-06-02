920c3bc7979029b24075d96de28ce1d928e4dfb4
Vishota <88852731+Vishota@users.noreply.github.com>
2025-06-02 15:56:52 +0300

    1.0.2. html formater changed

--- src/formatters/Html.ts
+++ src/formatters/Html.ts
@@ -4,8 +4,8 @@ import { escapeHTML } from '../utils';
 
 export class HtmlFormatter implements IFormatter {
     format(entries: FileEntry[]): string {
-        const header = `<!DOCTYPE html><html><head><meta charset="UTF-8"><title>vils Output</title></head><body><h1>vils@${require('../../package.json').version}</h1>`;
-        const footer = `</body></html>`;
+        const header = `<!DOCTYPE html><html><head><meta charset="UTF-8"><title>vils Output</title></head><body><pre>vils@${require('../../package.json').version}</pre>`;
+        const footer = `<pre><a href="https://github.com/Vishota/vils">https://github.com/Vishota/vils/</a></pre></body></html>`;
         const body = entries.map(e => {
             const safeName = escapeHTML(e.relativePath);
             if (typeof e.content === 'string') {
